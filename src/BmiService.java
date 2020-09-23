d512eac6425387905b4a2d6fc40dcaba324ad250
melomanos <philippov_mihail@mail.ru>
2020-09-23 21:18:33 +0300

    Rework

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,6 @@
 public class BmiService {
     public float calculate(float weight, float height) {
         float bmi = weight / (height * height);
-        bmi = Math.round(bmi);
 
         return bmi;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        float weight = (float) 85;
-        float height = (float) 1.8;
+        float weight = 85F;
+        float height = 1.8F;
         float bmi = service.calculate(weight, height);
 
-        System.out.println(bmi);
+        System.out.printf("Индекс массы тела %.2f", bmi);
     }
 }
